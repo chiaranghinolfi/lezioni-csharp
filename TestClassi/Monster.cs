aab11414d54836bb669703ca19780bcd20ab9359
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-11-28 11:09:12 +0100

    aggiunta funzione attacca

--- TestClassi/Monster.cs
+++ TestClassi/Monster.cs
@@ -6,13 +6,15 @@ namespace TestClassi
     class Monster
     {
         public string name;
-        public int hp;
+        public int maxHp;
+        public int curHp;
         public int damage;
         
-        public Monster(string name, int hp, int damage)
+        public Monster(string name, int maxHp, int damage)
         {
             this.name = name;
-            this.hp = hp;
+            this.maxHp = maxHp;
+            curHp = maxHp;
             this.damage = damage;
             describe();
         }
@@ -20,7 +22,7 @@ namespace TestClassi
         public string describe()
         {
             string output = "Questo è " + name + "\r\n";
-            output += "HP: " + hp + "\r\n";
+            output += "HP: " + curHp + "\r\n";
             output += "DAMAGE: " + damage + "\r\n";
 
             return output;
@@ -29,6 +31,28 @@ namespace TestClassi
         public void describe(TextBox t)
         {
             t.Text = describe();
+        }
+
+        public void attack(Monster target)
+        {
+            if (target.curHp <= 0)
+            {
+                Console.WriteLine(target.name + " è già esausto, non infierire.");
+                return;
+            }
+
+            Console.WriteLine(name + " attacca " + target.name);
+            Console.WriteLine(name + " fa " + damage + " danni a " + target.name);
+            target.curHp -= damage;
+            
+            if (target.curHp <= 0 )
+            {
+                target.curHp = 0;
+                Console.WriteLine(target.name + " è esausto.");
+            } else
+            {
+                Console.WriteLine("a " + target.name + " rimangono " + target.curHp + " hp");
+            }
         }
     }
 }
