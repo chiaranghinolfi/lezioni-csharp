3c3eceae2f435765d95a000b92b10fbe3e49927d
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-12-19 09:54:09 +0100

    eredit

--- TestClassi/Fire_Monster.cs
+++ TestClassi/Fire_Monster.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace TestClassi
 {
-    class Fire_Monster
+    class Fire_Monster : Monster
     {
+        public int fireDamage { get; set; }
+
+        public Fire_Monster(string name, int maxHp, int damage, int fireDamage) : base(name, maxHp, damage, 0)
+        {
+            this.fireDamage = fireDamage;
+        }
     }
 }
